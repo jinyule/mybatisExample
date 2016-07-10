2556d489679d0e6dd2d48b49b599ecb964ce0f0a
jinyule <gkimfdqqqq@gmail.com>
2016-07-10 22:55:44 +0800

    xx

---
+++ src/main/java/com/huawei/neteco/pm/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.huawei.neteco.pm.mapper;
+
+import com.huawei.neteco.pm.model.User;
+
+/**
+ * Created by terminator on 2016/5/20.
+ * User: terminator
+ * Date: 2016/5/20
+ * Time: 22:27
+ * Copyright
+ */
+public interface UserMapper {
+
+    public User getUser(Integer id);
+}
---
+++ src/main/java/com/huawei/neteco/pm/model/User.java
@@ -0,0 +1,30 @@
+package com.huawei.neteco.pm.model;
+
+/**
+ * Created by terminator on 2016/5/20.
+ * User: terminator
+ * Date: 2016/5/20
+ * Time: 22:24
+ * Copyright
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
